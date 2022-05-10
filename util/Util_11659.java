8bb875adcb075409680fd5877ec83577ce6bd94b
82102 <sms7624@gmail.com>
2022-05-10 23:40:32 +0900

    java algorithm

---
+++ util/Util_11659.java
@@ -0,0 +1,34 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Util_11659 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static int n, m, start, end;
+    static long[] S;
+    public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+	
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		S = new long[n + 1];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= n; i++) {
+			S[i] = S[i - 1] + Integer.parseInt(st.nextToken());
+		}
+		while(m-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			start = Integer.parseInt(st.nextToken());
+			end = Integer.parseInt(st.nextToken());
+			sb.append(S[end] - S[start - 1]).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
---
+++ util/Util_1940.java
@@ -0,0 +1,42 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Util_1940 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		int[] num = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++)
+		{
+			num[i] = Integer.parseInt(st.nextToken());	
+		}
+		Arrays.sort(num);
+		int start = 0;
+		int end = n - 1;
+		int sum;
+		int count = 0;
+		while(start < end)
+		{
+			sum = num[start] + num[end];
+			if (sum == m) {
+				start++;
+				end--;
+				count++;
+			} else if (sum > m) {
+				end--;
+			} else {
+				start++;
+			}
+		}
+		System.out.println(count);
+	}
+}
